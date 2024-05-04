4c243fae4f665deb8502e31cdab60d280058a3bc
tuOrzech <142775512+tuOrzech@users.noreply.github.com>
2024-05-04 12:03:02 +0200

    add script for AWS CodeArtifact auth_token

--- examples/module4/lesson2/design-tokens/update_token.sh
+++ examples/module4/lesson2/design-tokens/update_token.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-new_token=$(aws codeartifact get-authorization-token --domain przeprogramowani-ofe --domain-owner 438634852085 --query authorizationToken --output text)
+new_token=$(aws codeartifact get-authorization-token --domain tuorzech --domain-owner 590183693949 --query authorizationToken --output text)
 
 sed -i "" "s|authToken=.*|authToken=$new_token|" .npmrc
 
